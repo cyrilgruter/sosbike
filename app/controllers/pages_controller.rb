40b24ead3d498461df83d50b1b6bcd526c974a65
Matthias Vitte <vittematthias@gmail.com>
2016-02-24 15:25:01 +0100

    selection of the area from the autocomlete date is made

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,17 @@ class PagesController < ApplicationController
   end
 
   def problem
-    raise
     #@adress = params[:adress]
+    @postal_code = params[:postal_code]
+    if @postal_code == ""
+      flash[:alert] = "Veuillez entrer un code postal"
+      render :home
+    elsif @postal_code == "75011"
+
+    else
+      flash[:alert] = "Désolé, vous devez être dans le 11ème"
+      render :home
+    end
+
   end
 end
