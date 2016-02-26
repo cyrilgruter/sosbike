aa42598204e21d94e55f4115ce6a5bfd317d029c
Laure Leter <laure.leter@gmail.com>
2016-02-26 09:42:43 +0100

    fixed user complementary information with devise

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -1,5 +1,5 @@
 class Repair < ActiveRecord::Base
-  CATEGORIES = ["Flat tire", "Worn brakes", "Broken chain"]
+  CATEGORIES = ["flat tire", "worn brakes", "broken chain"]
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
   belongs_to :client, class_name: 'User'
