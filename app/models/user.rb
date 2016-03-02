d15f7349d233fea0022862999085cb133d9d4893
cyrilgruter <cyrilgruter@hotmail.com>
2016-03-02 17:38:26 +0100

    phone regex + format + home page font size

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   has_many :missions, class_name: 'Repair', foreign_key: 'saver_id'
   has_many :reviews
   has_attachment :photo
+  validates :first_name, presence: true
+  validates :phone, format: { with: /\A((\+|00)33\s?|0)[1-9](\s?\d{2}){4}\Z/,
+                              message: "wrong phone number format"}
 end
